4978853deda24732204dbe727a5cb67a53d3a4ba
Alan Wong <captainalan@gmail.com>
2019-01-09 22:01:35 -0800

    Updates

---
+++ precache-manifest.374e9047b95149948d7754375dee3ecd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c5a1cf98fb440b3d10a",
+    "url": "/me/static/css/main.4937d2ba.chunk.css"
+  },
+  {
+    "revision": "9c5a1cf98fb440b3d10a",
+    "url": "/me/static/js/main.9c5a1cf9.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/me/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "f4ae667e0f9864d95541",
+    "url": "/me/static/js/runtime~main.f4ae667e.js"
+  },
+  {
+    "revision": "630e7cd16167d1fe02151a6bec9ced90",
+    "url": "/me/static/media/github_profile.630e7cd1.png"
+  },
+  {
+    "revision": "7183317e8247b508b1261d8e95274e7a",
+    "url": "/me/static/media/2015 mothers day card.7183317e.png"
+  },
+  {
+    "revision": "fb4a4b59b07a6de9c3cb6a094a754071",
+    "url": "/me/static/media/egghead-davis.fb4a4b59.jpg"
+  },
+  {
+    "revision": "3d88e20429c56957285f76b9ca8d2fe5",
+    "url": "/me/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2094c00f5bba8f45feb8762bfebe0397.js"
+  "/me/precache-manifest.374e9047b95149948d7754375dee3ecd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/me/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

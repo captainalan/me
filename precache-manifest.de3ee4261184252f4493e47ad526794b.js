ec157458d57ae5da63918a465b3e54e6783bf6e2
Alan Wong <captainalan@gmail.com>
2020-07-07 19:10:02 -0700

    Updates

---
+++ precache-manifest.de3ee4261184252f4493e47ad526794b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a4a20397cc4e7c1b48e5e50e32124e1",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "5c8102f39c690ce40e72",
+    "url": "/me/static/css/main.f6f8aad2.chunk.css"
+  },
+  {
+    "revision": "3951e962b6f17210bd3e",
+    "url": "/me/static/js/2.cc500341.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me/static/js/2.cc500341.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c8102f39c690ce40e72",
+    "url": "/me/static/js/main.febff3c4.chunk.js"
+  },
+  {
+    "revision": "73563d41566289fc5215",
+    "url": "/me/static/js/runtime-main.aea40d98.js"
+  },
+  {
+    "revision": "7183317e8247b508b1261d8e95274e7a",
+    "url": "/me/static/media/2015 mothers day card.7183317e.png"
+  },
+  {
+    "revision": "fb4a4b59b07a6de9c3cb6a094a754071",
+    "url": "/me/static/media/egghead-davis.fb4a4b59.jpg"
+  },
+  {
+    "revision": "630e7cd16167d1fe02151a6bec9ced90",
+    "url": "/me/static/media/github_profile.630e7cd1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.a5c570b833f9ef9c7b99b5d2844c3d1c.js"
+  "/me/precache-manifest.de3ee4261184252f4493e47ad526794b.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/me/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/me/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
